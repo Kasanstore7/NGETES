4ad89b892b68789062f3bce4092e3e260f8d1c0f
HaqzStore <haqzganz@gmail.com>
2022-10-01 13:41:40 +0700

    scnoenc

---
+++ plugins/_anticulik.js
@@ -0,0 +1,45 @@
+let fs = require('fs')
+let handler = m => m
+
+handler.all = async function (m, { isBlocked }) {
+    if (isBlocked) return
+    // ketika ada yang invite/kirim link grup di chat pribadi
+    if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
+    let teks = `「 Herobot 」
+
+    ╭━━━━「 SEWA 」
+    ┊⫹⫺ Hemat: 5k/grup (1 minggu)
+    ┊⫹⫺ Normal: 15k/grup (1 bulan)
+    ┊⫹⫺ Standar: 30k/grup (2 bulan)
+    ┊⫹⫺ Pro: 35k/grup (4 bulan)                                                      
+    ┊⫹⫺ Vip: = 65k/grup (12 bulan)
+    ╰═┅═━––––––๑
+    
+    ╭━━━━「 PREMIUM 」
+    ┊⫹⫺ Hemat: 5k (1 minggu)
+    ┊⫹⫺ Normal: 20k (1 bulan)
+    ┊⫹⫺ Pro: 40k (4 bulan)
+    ┊⫹⫺ Vip: 50k (8 bulan)                                               
+    ┊⫹⫺ Permanent: = 70k (Unlimited)
+    ╰═┅═━––––––๑
+    
+    ⫹⫺ PAYMENT:
+    • Pulsa Telkomsel: [081333741517]
+    • Dana: [08155174757]
+    • Gopay: [081333741517]
+    • Ovo: [081333741517]
+    • Link Aja: [08155174757]
+    
+    Nomor Owner :
+    wa.me/628155174757
+    
+    ▌│█║▌║▌║║▌║▌║█│▌
+    
+    #BY HaqzzXD`
+    this.reply(m.chat, teks, m)
+    const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+    this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+    }
+}
+
+export default handler
---
+++ plugins/donasi.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+┌─「 Donasi • Pulsa 」
+│ • *Indosat:* [${global.ppulsa}]
+│ • *Telkomsel:* [${global.ppulsa2}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+│ • *Gopay:* [${global.pgopay}]
+│ • *Ovo:* [${global.povo}]
+│ • *Link Aja:* [${global.plinkaja}]
+❏────
+`.trim()
+  m.reply(text)
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
